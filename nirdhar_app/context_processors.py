bee70cfcd3fe92e1a5b450d025c421098292d6aa
lioaraxai <lioaraxai@gamil.com>
2025-04-25 09:14:30 +0200

    Renamed project from Nirdhar to LioraXAI

--- nirdhar_app/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .forms import ContactForm, SubscriberForm, DemoRequestForm
-
-def common_forms(request):
-    return {
-        'contact_form': ContactForm(),
-        'subscriber_form': SubscriberForm(),
-        'demo_form': DemoRequestForm(),
-    } 
