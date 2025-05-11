ec98ad5ac74f09165f429ef1f7eda54252dd4d9c
lioaraxai <lioaraxai@gamil.com>
2025-05-11 22:29:30 +0200

    Automatically create superuser during build

--- build.sh
+++ build.sh
@@ -14,4 +14,7 @@ chmod +x setup.sh
 python manage.py collectstatic --noinput
 
 # Run migrations
-python manage.py migrate 
+python manage.py migrate
+
+# Create a superuser if none exists
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'Lioraxai@123!') if not User.objects.filter(username='admin').exists() else print('Admin user already exists')" | python manage.py shell 
